b24df206884d2c719eee128c93622fab0b3e5f10
MaitreYoda <younes.chiboub@devinci.fr>
2019-02-22 19:24:42 +0100

    docs(readme): add usage section

--- cash/README.md
+++ cash/README.md
@@ -1,1 +1,69 @@
 # cash
+
+cash
+Features
+Cash is a library which converts the amounts of money given in different currencies.
+By default, if you put one currency
+
+Getting started
+Clone the project
+npm install
+
+Commands
+Convert an amount of money from a currency to another one (or many) :
+node index.js 
+By default, if you don't give the final currency, it will convert into euro, US dollar and Pound Sterling.
+If you don't give any parameter, it will convert from US dollar to euro and Pound Sterling.
+
+--save, -s Save currencies as default currencies
+--help, -h Display help message
+--version, -v Display version number
+
+Examples:
+node index.js 1 CAD EUR 
+result : 
+√1.60 (CAD) Canadian Dollar
+
+node index.js 100
+result :
+√80.85 (EUR) Euro
+√71.65 (GBP) Pound Sterling node index.js 1 EUR CNY RUB
+result :
+√ 7.81 (CNY) Chinese Yuan
+√ 70.49 (RUB) Russian Rouble
+
+Currencies allowed
+"AUD": "Australian Dollar",
+"RUB": "Russian Rouble",
+"EUR": "Euro",
+"BGN": "Bulgarian Lev",
+"BRL": "Real Brazilian",
+"CAD": "Canadian Dollar",
+"CHF": "Swiss Franc",
+"CNY": "Chinese Yuan",
+"CZK": "Czech Koruna",
+"DKK": "Danish Krone",
+"GBP": "Pound Sterling",
+"HKD": "Hong Kong Dollar",
+"HRK": "Croatian Kuna",
+"HUF": "Hungarian Forint",
+"IDR": "Indonesian Rupiah",
+"ILS": "Israeli Shekel",
+"INR": "Indian Rupee",
+"JPY": "Japanes Yen",
+"KRW": "South Korean Won",
+"MXN": "Mexican Peso",
+"MYR": "Malaysian Ringgit",
+"NOK": "Norwegian Krone",
+"PHP": "Philippine Peso",
+"PLN": "Polish Zloty",
+"RON": "Romanian New Leu",
+"SEK": "Swedish Krona",
+"SGD": "Singapore Dollar",
+"THB": "Thai Baht",
+"TRY": "Turkish Lira",
+"USD": "US Dollar",
+"ZAR": "South African Rand",
+"NZD": "New Zealand Dollar"
+Licence
+None
--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -2,30 +2,24 @@ const starWars = require('./index');
 const starWarsNames = require('./starwars-names.json');
 
 describe('starwars-names', () => {
-  describe('all', () => {
-    test('should be a fulfilled array', () => {
-      expect(starWars.all).toHaveLength(starWarsNames.length)
-    });
-    test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
+    describe('all', () => {
+        test('should be a fulfilled array', () => {
+            expect(starWars.all).toHaveLength(starWarsNames.length)
+        });
+        test('should be an array of strings', () => {
+            for (var i = 0; i < starWarsNames.length; i++) {
+                expect(typeof "starWarsNames[i]").toBe("string");
+            }
+        });
+        test('should contain `Luke Skywalker`', () => {
+            expect(starWarsNames).toEqual(expect.arrayContaining(["Luke Skywalker"]));
 
-    test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
+        });
 
-    test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
-  });
+        test('should not contain `Ben Quadinaros`', () => {
+            expect(starWarsNames) != (expect.arrayContaining(["Ben Quadinaros"]));
 
-  describe('random', () => {
-    test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+        });
     });
-
-    test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
-  });
+   
 });
